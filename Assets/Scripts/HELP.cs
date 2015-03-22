0857e66fdd7988f2f4394bc2cceb97ce2c22a9fc
Joseph McConnell <jjmdevelops@gmail.com>
2015-03-22 15:06:12 -0400

    I made the popup menu show up in the middle of the screen, and added a working close button to it

--- Assets/Scripts/HELP.cs
+++ Assets/Scripts/HELP.cs
@@ -5,7 +5,9 @@ public class HELP : MonoBehaviour {
 
 	public bool doWindow0 = false;
 	void DoWindow0(int windowID) {
-		GUI.Button(new Rect(10, 30, 80, 20), "Click Me!");
+		if (GUI.Button (new Rect (10, 30, 80, 20), "Close")) {
+			doWindow0 = false;
+		}
 	}
 
 	// Use this for initialization
@@ -26,10 +28,12 @@ public class HELP : MonoBehaviour {
 	void OnGUI() {
 		//doWindow0 = GUI.Toggle(new Rect(10, 10, 100, 20), doWindow0, "Window 0");
 		if (doWindow0) {
-			GUI.Window (0, new Rect (110, 10, 200, 60), DoWindow0, "Basic Window");
+			GUI.Window (0, new Rect (Screen.width/2, Screen.height/2, 200, 60), DoWindow0, "Basic Window");
 		}
-		
+
 	}
+		
+	
 
 
 
