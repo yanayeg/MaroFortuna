936c799d15ffdac03f1b72faf65b97160c3627c9
craigwilliams9992 <craigwilliams@ufl.edu>
2015-03-25 12:42:08 -0400

    Beginning class structures, Character class
    Forgot to commit on monday, kill me nowwwwww

---
+++ Assets/Scripts/Global Classes/Character.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public int profession;    //1 = Espionage, 2 = Scientist, 3 = Diplomat, 4 = Military
+	public string charName;       //character name
+	public GameObject sprite;
+	public double experience;
+	public int status;        //0 = Dead, 1 = alive, 2 = injured
+	public string description;
+
+	public Character() {
+		//empty constructor
+	}
+
+	public Character(int profession, string charName, GameObject sprite, double experience, int status, string description) {
+		this.profession = profession;
+		this.charName = charName;
+		this.sprite = sprite;
+		this.experience = experience;
+		this.status = status;
+		this.description = description;
+	}
+
+	public void setStatus(int status){
+		this.status = status;
+	}
+
+	public void addExperience(int expGain){
+		this.experience += expGain;
+	}
+	GameObject spriteChar1;
+	void start(){
+
+		//constructing one character, I am not sure how to assign the sprite image yet, this is my best guess.
+		//What I think happens is when you add this script to the static script empty game object in our first scene (the on that we dont destroy)
+		//it will give us fields with all these unassigned GameObject sprite variables where we will assign the images.
+		//GameObject spriteChar1;
+	Character char1 = new Character (1, "Jorts Bear", spriteChar1, 0, 1,
+		                                "A rogue cop who doesn't play by the rules... in jorts.");
+
+
+
+
+
+		Character[] availableCharcters = {char1};        //list of available characters
+		Character[] squad = new Character[availableCharcters.Length]; //creates an empty squad list the size of our availableCharacter list
+
+
+	}
+}
---
+++ Assets/Scripts/Global Classes/TestCharcterClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCharcterClass : MonoBehaviour {
+
+		
+		public int profession1;    //1 = Espionage, 2 = Scientist, 3 = Diplomat, 4 = Military
+		public string charName1;       //character name
+		public GameObject sprite1;
+		public double experience1;
+		public int status1;        //0 = Dead, 1 = alive, 2 = injured
+		public string description1;
+
+
+}
