fe1f23cdee5b83d1109dfc31f9cbc218a5053682
ddachille <ddachille@ufl.edu>
2015-03-26 02:01:39 -0400

    testing results example

--- Assets/Scripts/testTextScript.cs
+++ Assets/Scripts/testTextScript.cs
@@ -9,10 +9,17 @@ public class testTextScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		var AVariable = "FUCK UNITY";
+		var AVariable = "YOU HAVE SUCCESSFULLY COMPLETED THE MISSION!";
 		Text guiText = GameObject.Find("successMessage").GetComponent<Text>();
 		guiText.text = AVariable;
 	
+		var AVariable1 = "No one died yay omg okay you successfully killed all the space pirates";
+		Text guiText1 = GameObject.Find("Description").GetComponent<Text>();
+		guiText1.text = AVariable1;
+
+		//var AVariable2 = "YOU HAVE SUCCESSFULLY COMPLETED THE MISSION!";
+		//Text guiText2 = GameObject.Find("successMessage").GetComponent<Text>();
+		//guiText2.text = AVariable2;
 	}
 	
 	// Update is called once per frame
