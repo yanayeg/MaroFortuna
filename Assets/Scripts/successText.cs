db057718bb0b9323ab2a22e838c1a5527919ff78
Jasmine Perez <jasmineperez24@gmail.com>
2015-04-14 15:26:09 -0400

    adds a new character after a successful mission and an even day.

--- Assets/Scripts/successText.cs
+++ Assets/Scripts/successText.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class successText : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-		
+		Character rewardChar;
 		string success = Data.pickedMission.successDesc;
 		string fail = Data.pickedMission.failDesc;
 		bool win = false;
@@ -20,6 +20,10 @@ public class successText : MonoBehaviour {
 				Data.activeMissionChars[i].addExperience(100);
 
 			guiText1.text = "Success \n" + success + "\n\nRewards: \n" + Data.pickedMission.rewardRsc.rscName;
+			//grant character if it's a third day and successful
+			if (Data.dayCounter % 3 == 0 && Data.dayCounter != 0) 
+				rewardChar = rewardNewChar();
+			//if rewardChar isn't null fields alert the user about the new char
 		}
 		else
 			guiText1.text = "Fail \n" + fail;
@@ -47,6 +51,51 @@ public class successText : MonoBehaviour {
 
 		//update day counter
 		Data.dayCounter++;
+	}
+
+	Character rewardNewChar(){
+		Character rewardChar = new Character();
+		List<Character> charsToPickFrom = Data.charList;
+		charsToPickFrom = ShuffleList(charsToPickFrom);
+		bool exists = false;
+		for (int i = 0; i < charsToPickFrom.Count ; i++) 
+		{
+			for(int j = 0; j < Data.currentChars.Count ; j++)
+			{
+				if (charsToPickFrom[i].name == Data.currentChars[j].name)
+				{
+					//already listed skip it
+					exists = true;
+					break;
+				}
+				else
+				{
+					exists = false;
+				}
+			}
+			
+			if(exists == false){
+				Data.currentChars.Add(charsToPickFrom[i]);
+				rewardChar = charsToPickFrom[i];
+				return rewardChar;
+			}
+		}
+		return rewardChar;
+	}
+	
+	private List<Character> ShuffleList(List<Character> inputList)
+	{
+		List<Character> randomList = new List<Character>();
+		System.Random r = new System.Random();
+		int randomIndex = 0;
+		while (inputList.Count > 0)
+		{
+			randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+			randomList.Add(inputList[randomIndex]); //add it to the new, random list
+			inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+		}
+		
+		return randomList; //return the new random list
 	}
 	
 	// Update is called once per frame
