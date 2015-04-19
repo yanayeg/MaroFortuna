57fa1b2aac8f2d8fbe946577222a11d59cdd5765
ddachille <ddachille@ufl.edu>
2015-04-19 18:04:35 -0400

    THE LAST HOPE

---
+++ Assets/Scripts/TriggeredEvent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggeredEvent : MonoBehaviour {
+	/*public bool doWindow1 = false;
+
+	// Use this for initialization
+	void Start () {
+		if(Data.dayCounter == 10){
+			doWindow1 = true;
+		}
+	}
+
+	void DoWindow1(int windowID) {
+			if (GUI.Button (new Rect (20, 160, 200, 20), "Accept")) {
+				doWindow1 = false;
+			}
+			else{
+			//print text?
+				//if(GUI.Button (new Rect (), ""))
+			}
+		 
+	}
+
+	void OnGUI() {
+		//doWindow0 = GUI.Toggle(new Rect(10, 10, 100, 20), doWindow0, "Window 0");
+		if (doWindow1) {
+			GUI.Window (0, new Rect (300, 100, 250, 200), DoWindow1, "TRIGGER!");
+		}
+		
+	}
+	// Update is called once per frame
+	//void Update () {
+
+	//}*/
+}
